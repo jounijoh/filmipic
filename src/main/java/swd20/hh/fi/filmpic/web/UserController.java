d45b9dce9e95c788de03cae09fa53f10dbefa5d9
Jouni Johansson <bgu211@myy.haaga-helia.fi>
2022-11-16 09:39:03 +0200

    save user

--- src/main/java/swd20/hh/fi/filmpic/web/UserController.java
+++ src/main/java/swd20/hh/fi/filmpic/web/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import swd20.hh.fi.filmpic.domain.User;
 import swd20.hh.fi.filmpic.domain.UserRepository;
@@ -13,34 +14,44 @@ import swd20.hh.fi.filmpic.domain.UserService;
 
 @Controller
 public class UserController {
-	@Autowired UserRepository userrepo;
-	@Autowired UserService service;
-	
+	@Autowired
+	UserRepository userrepo;
+	@Autowired
+	UserService service;
+
 	// List All Users
 	@GetMapping("/allusers")
 	@PreAuthorize("hasAuthority('ADMIN')")
 	public String getAllusers(Model model) {
 		model.addAttribute("users", userrepo.findAll());
-		
+
 		return "userlist";
 	}
-	
+
 	// Create new User
 	@GetMapping("/createuser")
 	public String createUser(Model model) {
 		User user = new User();
 		model.addAttribute(user);
-		
+
 		return "newuser";
 	}
-	/*Show user Details
-	@GetMapping("/userdetails")
-	public String showUserDetails(@AuthenticationPrincipal ShopmeUserDetails userDetails,
-            Model model) {
-		String userName = userDetails.getUsername();
-		User user = service.getByUsername(userName);
-		model.addAttribute("user", user);
-		return "userdetails";
+
+	// Save user
+	@PostMapping("/saveuser")
+	public String saveUser(Model model, User user) {
+		user.setRole("USER");
+		userrepo.save(user);
+		return "redirect:/allphotos";
 	}
-	*/
+
+	/*
+	 * Show user Details
+	 * 
+	 * @GetMapping("/userdetails") public String
+	 * showUserDetails(@AuthenticationPrincipal ShopmeUserDetails userDetails, Model
+	 * model) { String userName = userDetails.getUsername(); User user =
+	 * service.getByUsername(userName); model.addAttribute("user", user); return
+	 * "userdetails"; }
+	 */
 }
